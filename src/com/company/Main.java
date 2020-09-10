8e0acaa10b09d940628fae142f4cbb20188e74a3
LAPTOP-4VMLGF78\Islam Tharwat <islam.mohamed@student.kyh.se>
2020-09-10 13:44:10 +0200

    Revert "Modify"
    This reverts commit 5f4ad593a592c53699931c3b6e1ae5bfb4484d9c.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-     //Test 2
-        
+     //Test
+
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc( 1,  1, 3,  3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc( 1,  2,  3,  4));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(  1,  -2,  3,  -4));
